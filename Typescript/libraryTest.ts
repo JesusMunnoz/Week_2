682342f396e3aba1b57a8714c26bed2ae1b18ba3
JesusMunnoz <jesusmunozpost@gmail.com>
2024-04-01 12:10:14 +0200

    book - bookTest - library - libraryTest

---
+++ Typescript/libraryTest.ts
@@ -0,0 +1,15 @@
+import {Book} from "./book"
+import {Library} from "./library"
+
+let book1 : Book = new Book("Introducción a Javascript", 233, " 2344433-BC23333", "Joseph Smith", " Now Editions");
+let book2 : Book = new Book("La Bruja Mon", 64, " 2355533-BC255555", "Pilar Mateos", "SM");
+let bookLibrary : Library = new Library([], "Calle Cavanilles", "Perico Palotes");
+
+bookLibrary.toStringLibrary(book1);
+bookLibrary.toStringLibrary(book2);
+
+console.log("");
+console.log(bookLibrary.toStringlibroteca()); //Book1+ Address & Editor   Book2+ Address & Editor
+console.log(bookLibrary.getNumberOfBooks()); //2
+
+console.log(bookLibrary.findByAuthor("Pilar Mateos"));
